c343ba2b29aceb85c1b114990ffb77039edc1e70
major major <majorx234@gmail.com>
2020-06-27 23:02:14 +0200

    added a main() function wich is callable

--- add/add.py
+++ add/add.py
@@ -4,9 +4,13 @@ def add(x,y):
     print(x+y)
     return x+y    
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) == 3:
         add(int(sys.argv[1]),int(sys.argv[2]))
         #return (int(sys.argv[1])+int(sys.argv[2]))
     else:
         print("ERROR:",str(len(sys.argv)-1)," arguments given instead of 2")
+
+if __name__ == "__main__":
+    main()
+
