1de45e675a6f4e72d2083889a893701c61715b45
major major <majorx234@gmail.com>
2020-06-28 02:26:25 +0200

    removed printf

--- add/add.py
+++ add/add.py
@@ -1,12 +1,11 @@
 import sys
 
 def add(x,y):
-    print("add: %d" % x+y)
     return x+y    
 
 def main():
     if len(sys.argv) == 3:
-        add(int(sys.argv[1]),int(sys.argv[2]))
+        print(add(int(sys.argv[1]),int(sys.argv[2])))
         #return (int(sys.argv[1])+int(sys.argv[2]))
     else:
         print("ERROR:",str(len(sys.argv)-1)," arguments given instead of 2")
